017389a9287c0cdc8e056638dc7625d9ebc9bea9
benjaminpbritcliffe-netizen <benjamin.p.britcliffe@gmail.com>
2025-09-02 14:36:39 +0100

    GoogleSearch Python Script changes

--- Python/PCL-Code-Repository-1/Code Portal/Google Search Script/GoogleSearch.py
+++ Python/PCL-Code-Repository-1/Code Portal/Google Search Script/GoogleSearch.py
@@ -3,4 +3,4 @@ import urllib.parse
 google = os.popen('zenity --entry --text="Enter your Google search: " --title="Search"').read()
 google = urllib.parse.quote(google)
 os.system('chromium-browser http://www.google.com/search?q=%s' % (google))
-                                                         
+
